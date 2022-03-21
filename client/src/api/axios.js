b7074b4c45c1bb73d2ca75126f02ceb0e56bca13
John Paul Lopez <jpblopez05@gmail.com>
2022-03-21 15:00:32 +0800

    changed to use env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_URL
+
 export default axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
   withCredentials: true,
 });
