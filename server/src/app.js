4b8b6767adbcc0ab1575061bf06a2a7b5399b06c
John Paul Lopez <jpblopez05@gmail.com>
2022-03-22 17:54:19 +0800

    refactoring the app structure

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const authRouter = require('./router/auth.js');
-const lodgingRouter = require('./router/lodging');
-const cookieParser = require('cookie-parser');
+const v1 = require('v1/index');
 
-const v2Router = require('./router/v2')
+const app = express();
+const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
-app.use('/v1/auth', authRouter);
-app.use('/v1/lodging', lodgingRouter);
-
-app.use('/v2', v2Router)
+app.use('/v1', v1);
 
 module.exports = app;
